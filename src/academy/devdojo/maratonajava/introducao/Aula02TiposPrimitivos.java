9b77cc95131a1ba74efef5666e3cabb88b69d301
ItaloBer <italoubernardo123@gmail.com>
2026-08-20 20:39:06 -0300

    firt commit

---
+++ src/academy/devdojo/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula02TiposPrimitivos {
+
+    public static void main (String[] args){
+        // Tipos primitivos
+        int age = (int) 2000000000000L;
+        long numeroGrande = 100000L;
+        double salary = 5000;
+        float salaryF = 5000;
+        byte idadeByte = 127;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'M';
+
+        // Tipos não primitivos
+
+        String nome = "GOkuu";
+
+
+        System.out.println("Your age : " + age);
+        System.out.println(numeroGrande);
+        System.out.println("Oi, meu nome é " + nome);
+
+
+    }
+
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,19 @@
+package academy.devdojo.maratonajava.introducao;
+
+
+
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+
+        String nome = "Italo";
+        String endereco = "1111 Davanuze";
+        double salario = 2000.50;
+        String dataRecebimentoSalario = "20/08/2006";
+        String relatorio = "Eu "+nome+", morando no endereço "+endereco+", confirmo que recebi o salário de " +salario+ ", na data de " + dataRecebimentoSalario;
+        System.out.println(relatorio);
+
+
+    }
+
+}
